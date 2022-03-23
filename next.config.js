7bb4a22e8a8e38b388bc6b8decb67125c0803b2c
Anudit Nagar <nagaranudit@gmail.com>
2022-03-23 22:01:44 +0530

    ⚡ Switch to PNPM & Preact

--- next.config.js
+++ next.config.js
@@ -9,7 +9,19 @@ module.exports = (phase) => {
     reactStrictMode: true,
     experimental: {
       esmExternals: false
-    }
+    },
+    webpack: (config, { dev, isServer }) => {
+      // Replace React with Preact only in client production build
+      if (!dev && !isServer) {
+        Object.assign(config.resolve.alias, {
+          react: 'preact/compat',
+          'react-dom/test-utils': 'preact/test-utils',
+          'react-dom': 'preact/compat',
+        });
+      }
+
+      return config;
+    },
   }
 
   if (phase === PHASE_DEVELOPMENT_SERVER) {
