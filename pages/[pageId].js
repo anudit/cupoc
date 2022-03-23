eb9013037b2636f1c47cf4dd3a74745745f043d6
Anudit Nagar <nagaranudit@gmail.com>
2022-03-23 22:09:18 +0530

    Update Error Handling

--- pages/[pageId].js
+++ pages/[pageId].js
@@ -6,28 +6,49 @@ import { Collection, CollectionRow, Modal, NotionRenderer } from 'react-notion-x
 export async function getServerSideProps(context) {
 
     const notion = new NotionAPI()
-    const recordMap = await notion.getPage(context.params.pageId);
+
+    let recordMap;
+    try {
+      recordMap = await notion.getPage(context.params.pageId);
+    } catch (error) {
+      recordMap = false;
+    }
 
     return { props: { recordMap } }
 }
 
 export default function Home({recordMap}) {
-  return (
-    <>
-      <Head>
-        <title>Careers - Cupoc, Inc.</title>
-        <meta name="description" content="Careers at Cupoc, Inc." />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-      </Head>
+  if (Boolean(recordMap) === false){
+    return (
+      <>
+        <Head>
+          <title>Not Found.</title>
+          <meta name="description" content="Not Found." />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-        <NotionRenderer components={ {
-            collection: Collection,
-            collectionRow: CollectionRow,
-            modal: Modal,
-        }} recordMap={recordMap} fullPage={true} darkMode={true} />
+        <code style={{color:'black'}}>Not Found.</code>
+      </>
+    )
+  }
+  else {
+    return (
+      <>
+        <Head>
+          <title>Careers - Cupoc, Inc.</title>
+          <meta name="description" content="Careers at Cupoc, Inc." />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+        </Head>
 
-    </>
-  )
+          <NotionRenderer components={ {
+              collection: Collection,
+              collectionRow: CollectionRow,
+              modal: Modal,
+          }} recordMap={recordMap} fullPage={true} darkMode={true} />
+
+      </>
+    )
+  }
 }
